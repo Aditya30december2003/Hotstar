73dcaad54ea3c340ea3ea48269dedc76fbb02374
Aditya Jain <94634290+Aditya30december2003@users.noreply.github.com>
2023-11-10 23:36:53 +0530

    Update Channels.jsx

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -11,28 +11,31 @@ import NationalVideo from '../assets/videos/national-geographic.mp4'
 import PixarVideo from '../assets/videos/pixar.mp4'
 import StarWars from '../assets/videos/star-wars.mp4'
 
-
 const Channels = (props) => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 mt-20 gap-6 max-w-[80vw] mx-auto'>
 
       <div className='shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] cursor-pointer hover:scale-110 duration-500 overflow-hidden relative'>
         <img src={Marvel} alt="" className='border-2 border-white rounded-[0.4rem] ring-inset block h-[100%] object-cover opacity-1  z-10 top-0'/>
-  
+        <video src={MarvelVideo} className='top-0 left-0 h-[100%] object-cover rounded-[0.4rem] absolute z-[-1] hover:z-[99]' preload='none' autoPlay muted loop></video>
       </div>
 
       <div className='shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] cursor-pointer hover:scale-110 duration-500 overflow-hidden relative'>
         <img src={Disney} alt="" className='border-2 border-white rounded-[0.4rem] ring-inset block h-[100%] object-cover opacity-1  z-10 top-0'/>
+        <video src={DisneyVideo} className='top-0 left-0 h-[100%] object-cover rounded-[0.4rem] absolute z-[-1] hover:z-[99]' preload='none' autoPlay muted loop></video>
       </div>
 
       <div className='shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] cursor-pointer hover:scale-110 duration-500 overflow-hidden relative'>
         <img src={National} alt="" className='border-2 border-white rounded-[0.4rem] ring-inset block h-[100%] object-cover opacity-1  z-10 top-0'/>
+        <video src={NationalVideo} className='top-0 left-0 h-[100%] object-cover rounded-[0.4rem] absolute z-[-1] hover:z-[99]' preload='none' autoPlay muted loop></video>
       </div>
       <div className='shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] cursor-pointer hover:scale-110 duration-500 overflow-hidden relative'>
         <img src={Pixar} alt="" className='border-2 border-white rounded-[0.4rem] ring-inset block h-[100%] object-cover opacity-1  z-10 top-0'/>
+        <video src={PixarVideo} className='top-0 left-0 h-[100%] object-cover rounded-[0.4rem] absolute z-[-1] hover:z-[99]' preload='none' autoPlay muted loop></video>
       </div>
       <div className='shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] cursor-pointer hover:scale-110 duration-500 overflow-hidden relative'>
         <img src={Star} alt="" className='border-2 border-white rounded-[0.4rem]  h-[100%] object-cover opacity-1  z-10 top-0'/>
+        <video src={StarWars} className='top-0 left-0 h-[100%] object-cover rounded-[0.4rem] absolute z-[-1] hover:z-[99]' preload='none' autoPlay muted loop></video>
       </div>
     </div>
   )
